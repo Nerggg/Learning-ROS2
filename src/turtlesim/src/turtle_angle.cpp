8c6e68a75afe7cc558618e3cbb719f57641b50af
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-10-25 17:11:43 +0700

    feat (turtle_angle): add the proper service server

--- src/turtlesim/src/turtle_angle.cpp
+++ src/turtlesim/src/turtle_angle.cpp
@@ -11,6 +11,11 @@ public:
         x_target_ = x;
         y_target_ = y;
 
+        // TODO: ganti turtlesim::srv::Theta dengan service yang sesuai karena asumsiku ini nama packageny turtlesim (bisa dilihat di folder srv terdapat Theta.srv)
+        service_ = this->create_service<turtlesim::srv::Theta>(
+            "turtle_angle", std::bind(&TurtleTrackerNode::thetaCallback, this, std::placeholders::_1, std::placeholders::_2)
+            )
+
         // Subscribe to the turtle's pose
         turtle_pose_sub_ = this->create_subscription<geometry_msgs::msg::Pose>(
             "turtle1/pose", 10, std::bind(&TurtleTrackerNode::poseCallback, this, std::placeholders::_1)
@@ -18,22 +23,62 @@ public:
     }
 
 private:
+  void thetaCallback(const std::shared_ptr<turtlesim::srv::Theta::Request> request, std::shared_ptr<turtlesim::srv::Theta::Response> response) {
+      RCLCPP_INFO(this->get_logger(), "Request: x=%f, y=%f", request->x, request->y);
+
+      float goal_x = request->x;
+      float goal_y = request->y;
+
+      // pendekatan ini gembel tapi ngehandle kuadran tiga
+      if (goal_x - this->x_turtle == 0) {
+        if (goal_y - this->y_turtle > 0) {
+          response->theta = 1.5708;
+        } else {
+          response->theta = -1.5708;
+        }
+      } else {
+        response->theta = atan((goal_y - this->y_turtle) / (goal_x - this->x_turtle));
+      }
+
+      if (goal_x - this->x_turtle < 0 && goal_y - this->y_turtle < 0) {
+        response->theta += 3.14159;
+      }
+
+      if (response->theta < 0) {
+        response->theta += 6.28319;
+      }
+      if (response->theta > 6.28319) {
+        response->theta -= 6.28319;
+      }
+
+      RCLCPP_INFO(this->get_logger(), "Sending back response: [%f]", response->theta);
+  }
+
     void poseCallback(const geometry_msgs::msg::Pose::SharedPtr pose_msg) {
-        double x_turtle = pose_msg->position.x;
-        double y_turtle = pose_msg->position.y;
+      x_turtle = pose_msg->x;
+      y_turtle = pose_msg->y;
 
-        // Calculate the angle between turtle and (x, y) using atan2
-        double angle_rad = std::atan2(y_target_ - y_turtle, x_target_ - x_turtle);
-        double angle_deg = angle_rad * 180.0 / M_PI;
+        // Pendekatan ini bisa salah karena tidak memperhitungkan kuadran ketiga (x < 0, y < 0)
+//        // Calculate the angle between turtle and (x, y) using atan2
+//        double angle_rad = std::atan2(y_target_ - y_turtle, x_target_ - x_turtle);
+//        double angle_deg = angle_rad * 180.0 / M_PI;
 
-        std::cout << "Turtle is at (" << x_turtle << ", " << y_turtle << ")" << std::endl;
-        std::cout << "Angle to target (" << x_target_ << ", " << y_target_ << "): " << angle_deg << " degrees" << std::endl;
+      // Ini kuhilangkan biar ga kebanyakan outputnya
+//        std::cout << "Turtle is at (" << x_turtle << ", " << y_turtle << ")" << std::endl;
+//        std::cout << "Angle to target (" << x_target_ << ", " << y_target_ << "): " << angle_deg << " degrees" << std::endl;
 
     }
 
     rclcpp::Subscription<geometry_msgs::msg::Pose>::SharedPtr turtle_pose_sub_;
-    double x_target_;
-    double y_target_;
+    rclcpp::Service<turtlesim::srv::Theta>::SharedPtr service_; // TODO: ganti juga ini turtlesim::srv::Theta dengan yang sesuai
+
+    // x_target dan y_target nanti dimasukkan di request dari service
+//    double x_target_;
+//    double y_target_;
+
+    // ku gini biar nanti bisa diakses di thetaCallback dengan lebih enak aja
+    double x_turtle;
+    double y_turtle;
 };
 
 int main(int argc, char** argv) {
